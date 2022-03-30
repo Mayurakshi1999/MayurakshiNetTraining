7bca4ad27371dcf9c5eac74018ad8f8fd0ba9850
Mayu <netgmayu@gmail.com>
2022-03-30 18:31:46 +0530

    commit5

---
+++ DAL/StudentDAL.cs
@@ -0,0 +1,140 @@
+﻿using BO;
+using DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DAL
+{
+    public class StudentDAL
+    {
+        private readonly studentEntities _db;
+        public StudentDAL()
+        {
+            _db = new studentEntities();
+        }
+        public StudentInfoResponse CreateStudent(StudentInfoRequest request)
+        {
+            StudentInfoResponse student = new StudentInfoResponse();
+            try
+            {
+                _db.Student1.Add(new Student1
+                {
+                    FirstName= request.Body.FName,
+                    LastName= request.Body.LName,
+                    RollNo = request.Body.RollNo,
+                    Marks = request.Body.Marks
+
+                });
+                _db.SaveChanges();
+                student.Header = new HeaderInfo
+                {
+                    CallStatus = "Success",
+                    TransactionID = request?.Header?.TransactionID
+                };
+
+                
+
+            }
+            catch(Exception ex)
+            {
+                student.Header = new HeaderInfo
+                {
+                    CallStatus = "Error",
+                    TransactionID = request?.Header?.TransactionID
+
+                };
+
+            }
+            return student;
+        }
+        public StudentInfoResponse GetStudentById(StudentInfoRequest request)
+        {
+            StudentInfoResponse student = new StudentInfoResponse();
+            try
+            {
+                var st = _db.Student1.Where(x => x.Id == request.Body.Id).FirstOrDefault();
+
+
+                student.Header = new HeaderInfo
+                {
+                    CallStatus = "Success",
+                    TransactionID = request?.Header?.TransactionID
+                };
+            }
+            catch (Exception ex)
+            {
+                student.Header = new HeaderInfo
+                {
+                    CallStatus = "Error",
+                    TransactionID = request?.Header?.TransactionID
+
+                };
+
+            }
+            return student;
+        }
+        public StudentInfoResponse UpdateStudent(StudentInfoRequest request)
+        {
+            StudentInfoResponse student = new StudentInfoResponse();
+            try
+            {
+
+                var st = _db.Student1.Where(x => x.RollNo == request.Body.RollNo).FirstOrDefault();
+                st.FirstName = request.Body.FName;
+                st.LastName = request.Body.LName;
+                st.Marks = (int?)request.Body.Marks;
+                _db.Student1.Add(st);
+                _db.Entry(st).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                student.Header = new HeaderInfo
+                {
+                    CallStatus = "Success",
+                    TransactionID = request?.Header?.TransactionID
+                };
+            }
+            catch (Exception ex)
+            {
+                student.Header = new HeaderInfo
+                {
+                    CallStatus = "Error",
+                    TransactionID = request?.Header?.TransactionID
+
+                };
+
+            }
+            return student;
+        }
+        public StudentInfoResponse DeleteStudent(StudentInfoRequest request)
+        {
+            StudentInfoResponse student = new StudentInfoResponse();
+
+            try
+            {
+                var st = _db.Student1.Where(x => x.RollNo == request.Body.RollNo);
+                _db.Student1.RemoveRange(st);
+                _db.SaveChanges();
+
+                student.Header = new HeaderInfo
+                {
+                    CallStatus = "Success",
+                    TransactionID = request?.Header?.TransactionID
+                };
+            }
+            catch (Exception ex)
+            {
+                student.Header = new HeaderInfo
+                {
+                    CallStatus = "Error",
+                    TransactionID = request?.Header?.TransactionID
+
+                };
+
+            }
+            return student;
+
+        }
+        
+    }
+}
---
+++ StudentWCFConsumer/Program.cs
@@ -0,0 +1,121 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StudentService;
+
+Console.WriteLine("Hello, World!");
+//var service = new StudenterviceClient();
+//var request = new StudentInfoRequest
+//{
+//    Header = new HeaderInfo
+//    {
+//        TransactionID = Guid.NewGuid().ToString()
+//    },
+//    Body = new StudentBO
+//    {
+//        //FirstName= "Mayu",
+//        //LastName= "Paul",
+//        //Marks = 100,
+//        //RollNo = 21
+//        FirstName = Console.ReadLine(),
+//        LastName = Console.ReadLine(),
+//        RollNo = Convert.ToInt32(Console.ReadLine()),
+//        Marks = Convert.ToInt32(Console.ReadLine())
+
+//    }
+
+//};
+//var response = service.CreateStudent(request);
+//Console.WriteLine(response.Header.TransactionID);
+//Console.WriteLine(response.Header.CallStatus);
+int size = 0;
+
+for (; ; )
+{
+    Console.WriteLine("1. Create Student.");
+    Console.WriteLine("2. Update Student Details.");
+    Console.WriteLine("3. Delete a Student by Roll.");
+
+
+    string input = Console.ReadLine();
+    if (input == "1")
+    {
+        Console.WriteLine("Enter the First name, last name, roll no and marks of the student");
+        var service = new StudenterviceClient();
+        var request = new StudentInfoRequest
+        {
+            Header = new HeaderInfo
+            {
+                TransactionID = Guid.NewGuid().ToString()
+            },
+            Body = new StudentBO
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                RollNo = Convert.ToInt32(Console.ReadLine()),
+                Marks = Convert.ToInt32(Console.ReadLine())
+            }
+
+        };
+        var response = service.CreateStudent(request);
+        Console.WriteLine(response.Header.TransactionID);
+        Console.WriteLine(response.Header.CallStatus);
+
+    }
+
+    //UPDATE METHOD
+    else if (input == "2")
+    {
+        Console.WriteLine("Enter the Roll No of the student record you want to edit:");
+        var Roll = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the changed First Name of the student:");
+        var FN = Console.ReadLine();
+        Console.WriteLine("Enter the changed Last Name of the student:");
+        var LN = Console.ReadLine();
+        Console.WriteLine("Enter the changed Marks :");
+        var M = Convert.ToInt32(Console.ReadLine());
+        var service = new StudenterviceClient();
+        var request = new StudentInfoRequest
+        {
+            Header = new HeaderInfo
+            {
+                TransactionID = Guid.NewGuid().ToString()
+            },
+            Body = new StudentBO
+            {
+                FirstName = FN,
+                LastName = LN,
+                RollNo = Roll,
+                Marks = M,
+            }
+        };
+
+
+        var response = service.UpdateStudent(request);
+        Console.WriteLine(response.Header.TransactionID);
+        Console.WriteLine(response.Header.CallStatus);
+
+    }
+
+    //DELETE METHOD
+    else if (input == "3")
+    {
+        Console.WriteLine("Enter the changed Roll Number of the student:");
+        var service = new StudenterviceClient();
+        var request = new StudentInfoRequest
+        {
+            Header = new HeaderInfo
+            {
+                TransactionID = Guid.NewGuid().ToString()
+            },
+
+            Body = new StudentBO
+            {
+
+                RollNo = Convert.ToInt32(Console.ReadLine()),
+            }
+        };
+
+        var response = service.DeleteStudent(request);
+        Console.WriteLine(response.Header.TransactionID);
+        Console.WriteLine(response.Header.CallStatus);
+    }
+}
