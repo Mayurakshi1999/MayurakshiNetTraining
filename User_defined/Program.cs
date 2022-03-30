3c2e1902fe2be11022e7e0347dc79b4c556a3349
Mayu <netgmayu@gmail.com>
2022-03-30 18:25:49 +0530

    commit2

---
+++ User_defined/Program.cs
@@ -0,0 +1,107 @@
+﻿using User_defined;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Linq;
+
+List<student> studentList = null;
+int size = 0;
+int x = 0;
+
+while (x>=0)
+{
+    Console.WriteLine("1. Enter Student Details.");
+    Console.WriteLine("2. Display Student Details.");
+    Console.WriteLine("3. Find a Name.");
+    String input = Console.ReadLine();
+    if (input == "1")
+    {
+        Console.WriteLine("Enter the number of students:");
+
+        int i = Convert.ToInt32(Console.ReadLine());
+        size = i;
+        studentList = new List<student>();
+
+        if (i != 0)
+        {
+            int j = 0;
+            while(j<i)
+            {
+
+                student student = new student();
+                Console.WriteLine($"Enter details for student{j + 1}");
+
+                Console.WriteLine("Enter First Name");
+                student.FirstName = Console.ReadLine();
+
+                Console.WriteLine("Enter LastName");
+                student.LastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Roll Number");
+                student.RollNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Marks Secured");
+                student.MarksSecured = Convert.ToDouble(Console.ReadLine());
+                studentList.Add(student);
+                j++;
+                
+            }
+        }
+
+    }
+
+    else if (input == "2")
+    {
+
+        Console.WriteLine("Thank You for sharing your Information. Your Details are as follows:");
+       
+            foreach (var student in studentList){
+                Console.WriteLine($"FirstName: {student.FirstName}");
+                Console.WriteLine($"LastName: {student.LastName}");
+                Console.WriteLine($"Roll Number: {student.RollNumber}");
+                Console.WriteLine($"Marks Secured: {student.MarksSecured}");
+            }
+    }
+    else if(input == "3")
+    {
+        Console.WriteLine("Enter the name"); 
+        string name = Console.ReadLine();
+        var res = from l in studentList
+                  where l.FirstName.Contains(name)
+                  select l;
+
+        // Executing LINQ Query
+        foreach (var q in res)
+        {
+            Console.WriteLine($"FirstName: {q.FirstName}");
+            Console.WriteLine($"LastName: {q.LastName}");
+            Console.WriteLine($"Roll Number: {q.RollNumber}");
+            Console.WriteLine($"Marks Secured: {q.MarksSecured}");
+        }
+    }
+
+
+
+    else
+    {
+        Console.WriteLine("Invalid Input");
+        break;
+    }
+    Console.WriteLine("Enter 1 to view main menu or 0 to exit");
+    int p = Convert.ToInt32(Console.ReadLine());
+    if (p == 1)
+    {
+        x++;
+    }
+    else
+    {
+        break;
+    }
+
+
+}
+
+
---
+++ User_defined/student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_defined
+{
+    public class student
+    {
+        private string _fristName;
+        private string _lastName;
+        private int _rollNumber;
+        private double _marksSecured;
+
+        public string FirstName
+        {
+            get
+            {
+                return _fristName;
+            }
+            set
+            {
+                _fristName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public int RollNumber
+        {
+            get
+            {
+                return _rollNumber;
+            }
+            set
+            {
+                _rollNumber = value;
+            }
+        }
+        public double MarksSecured
+        {
+            get
+            { 
+                return _marksSecured;
+            }
+            set
+            {
+                _marksSecured = value;
+            }
+        }
+
+
+
+    }
+}
